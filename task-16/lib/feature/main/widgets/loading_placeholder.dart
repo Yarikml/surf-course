5ab8ba830fc71a95a57b25fdcdbd9535247a5f6f
yaroslav <yaroslav.bunyaev@tularegion.ru>
2024-06-24 14:05:20 +0400

    add loader

--- task-16/lib/feature/main/widgets/loading_placeholder.dart
+++ task-16/lib/feature/main/widgets/loading_placeholder.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../../assets/resources/animations.dart';
 
 class LoadingPlaceholder extends StatelessWidget {
   const LoadingPlaceholder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(),
+    return Center(
+      child: Lottie.asset(Animations.loader),
     );
   }
 }
